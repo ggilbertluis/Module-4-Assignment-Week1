150893426fff4117db59dd6911f119756fb5b720
ggilbertluis <ggilbertluis@gmail.com>
2018-02-25 16:17:36 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,6 +4,6 @@ data<- as.data.table(data[data$Date == "1/2/2007" | data$Date == "2/2/2007", ])
 
 data$Global_active_power<- as.numeric(as.character(data$Global_active_power))
 
-png("plot1.png")
+png("plot1.png", width = 480, height = 480)
 hist(data$Global_active_power, col = "#FF2500",  ylim = c(0,1200), main = "Global Active Power", breaks = 12, xlab = "Global Active Power (kilowatts)" )
 dev.off()
